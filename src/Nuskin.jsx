c1e72ed7576ea844b52c2e26b3e5d6abd360ea05
acamm <aijun.camm@gmail.com>
2019-02-10 13:06:54 -0400

    Work in progress

--- src/Nuskin.jsx
+++ src/Nuskin.jsx
@@ -1,81 +1,142 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import './Nuskin.css';
+import App from 'App';
 import { Tab, Tabs, FormGroup, FormControl, ControlLabel, Checkbox, Button, ListGroup, ListGroupItem, InputGroup } from 'react-bootstrap'
-
+import ReactTable from "react-table";
+import 'react-table/react-table.css'
 
 class Nuskin extends Component {
-    
+   
   constructor(props) {
       super(props);
-      this.onSubmit = this.onSubmit.bind(this);
-      this.onIpAddrChange = this.onIpAddrChange.bind(this);
-      this.state = { ipaddress: ""}
+      this.onGetOrdersClick = this.onGetOrdersClick.bind(this);
+      this.onGetProductsClick = this.onGetProductsClick.bind(this);
+      this.renderEditable = this.renderEditable.bind(this);
+      this.handleDelete = this.handleDelete.bind(this);
+      this.handleEdit = this.handleEdit.bind(this);
+      const cadFormat = new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'CAD' })
+      
+      
+      this.orderColumns = [
+        { Header: "Orders", 
+           columns: [
+              { Header: 'Order Number',     accessor: 'orderNumber',      }, 
+              { Header: 'Date',             accessor: 'date',             },
+              { Header: 'Account',          accessor: 'account',          },
+              { Header: 'Subtotal',         accessor: 'subtotal', Cell: this.renderEditable },
+              { Header: 'Tax',              accessor: 'tax',      Cell: props => cadFormat.format(props.value) },
+              { Header: 'Shipping',         accessor: 'shipping', Cell: props => cadFormat.format(props.value) },
+              { Header: 'Shipping Address', accessor: 'shippingAddress',  },
+           ]
+        }
+      ];
+
+      this.productColumns = [
+        { Header: "Products", 
+          columns: [
+             { Header: 'Order Number',     accessor: 'orderNumber',  style: { textAlign: "left" }  }, 
+             { Header: 'Description',      accessor: 'description',  style: { textAlign: "left" }  }, 
+             { Header: 'Cost',             accessor: 'costPrice',    style: { textAlign: "right" }, Cell: cellInfo => cadFormat.format(cellInfo.value) }, 
+             { Header: 'Tax',              accessor: 'tax',          style: { textAlign: "right" }, Cell: cellInfo => cadFormat.format(cellInfo.value) }, 
+             { Header: 'Shipping',         accessor: 'shipping',     style: { textAlign: "right" }, Cell: cellInfo => cadFormat.format(cellInfo.value) },
+             { Header: '',
+                 Cell: row => (
+                     <div>
+                         <button onClick={() => handleEdit(row.original)}>Edit</button>
+                         <button onClick={() => handleDelete(row.original)}>Delete</button>
+                     </div>
+                 )
+              }
+          ]}
+      ];
+
+      this.columns = this.orderColumns;
+      
+      const ev = 1;
+      this.onGetOrdersClick(ev);
+      
+      
+      
   }
   
-  onIpAddrChange(ev) {
+  handleDelete(){
+      
   }
-    
-  onSubmit(ev) {
-      ev.preventDefault();
-      console.log("form onSubmit");
+  handleEdit(){
+      
   }
-    
-  render() {
+
+  renderEditable(cellInfo) {
+      const cadFormat = new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'CAD' })
 
       return (
-            
-        <form onSubmit={this.onSubmit} style={{margin: '10px'}}>
-            <FormGroup>
-               <InputGroup>
-               <InputGroup.Addon>Radio Type</InputGroup.Addon>
-                  <FormControl componentClass="select" >
-                    <option value="Any">Any</option>
-                    <option value="MSTRDU">MSTRDU</option>
-                    <option value="RRH2X">RRH2X</option>
-                    <option value="RFH">RFH</option>
-                  </FormControl>
-                </InputGroup>
-                <InputGroup>
-                    <InputGroup.Addon>IP Address</InputGroup.Addon>
-                    <FormControl type="text" value={this.state.ipaddress} onChange={this.onIpAddrChange} placeholder="192.168.1.1"/>
-                </InputGroup>
-           </FormGroup>
-           <Button>Connect</Button>
-         </form>
+        <div
+          style={{ backgroundColor: "#fafafa" }}
+          contentEditable
+          suppressContentEditableWarning
+          onBlur={e => {
+            const data = [...this.state.data];
+            data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
+            this.setState({ data });
+          }}
+          dangerouslySetInnerHTML={{
+            __html: cadFormat.format(this.state.data[cellInfo.index][cellInfo.column.id])
+          }}
+        />
+      );
+    }
+  
+    
+  onGetOrdersClick(ev) {
+      console.log("form onSubmit");
 
-/*       
-      <div className="Nuskin">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-      */
-       
+      this.columns = this.orderColumns;
+      
+      fetch("/orders")
+      .then( res => res.json())
+      .then( (res) => {this.update(res) } );
+      
+      
+  }
+  
+  onGetProductsClick(ev) {
+      this.columns = this.productColumns;
+      
+      fetch("/products")
+      .then( res => res.json())
+      .then( (res) => {this.update(res) } );
+      
+  }
+    
+  update(res) {
+      this.setState ({ data : res });
+  }
+  
+  
+  render() {
+
+      const data = this.state ? this.state.data : undefined;
+      
+      return (
+              <div>
+              <form style={{margin: '10px'}}>
+              <FormGroup>
+                 <InputGroup>
+                    <InputGroup.Prepend>Orders</InputGroup.Prepend>
+                    <Button onClick={this.onGetOrdersClick}>Get Orders</Button>
+                </InputGroup>
+                    <InputGroup>
+                    <InputGroup.Prepend>Products</InputGroup.Prepend>
+                    <Button onClick={this.onGetProductsClick}>Get Products</Button>
+                </InputGroup>
+             </FormGroup>
+           </form>         
+              <ReactTable data={data} columns={this.columns} defaultPageSize={10} className="-striped -highlight"  />
+              </div>
     );
   }
 }
-
-
-
-
-/*
-fetch("/orders")
-.then( res => res.json())
-.then( (res) => {this.plotMyData(res, resetScale) } );
-*/
-
 
 
 export default Nuskin;
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+module.exports = {
+		
+  mode: "development",
+  entry: {
+	    main:        './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'build/js'),
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: ['src', 'node_modules' ]
+  },
+  
+  module: {
+	    rules: [
+	      {
+	        test: /\.jsx$/,
+	        exclude: /node_modules/,
+	        use: {
+	          loader: "babel-loader"
+	        }
+	      },
+	      {
+	        test: /\.js$/,
+	        exclude: /node_modules/,
+	        use: {
+	          loader: "babel-loader"
+	        }
+	      },
+          {
+              test:/\.css$/,
+              use:['style-loader','css-loader']
+          }
+	    ]
+   },
+  
+  devtool: "source-map"
+
+};
+
