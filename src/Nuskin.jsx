dd4b8d6eb2d0dc8ef8b8e74e2e69a2425c000963
acamm <aijun.camm@gmail.com>
2019-03-07 12:36:27 -0400

    Update database with end use info

--- src/Nuskin.jsx
+++ src/Nuskin.jsx
@@ -3,90 +3,39 @@ import { Tab, Tabs, Form, FormGroup, FormControl, Control, ControlLabel, Checkbo
 import ReactTable from "react-table";
 import 'react-table/react-table.css'
 import './Nuskin.css';
+import axios from "axios";
+import Products from "Products";
 
 class Nuskin extends Component {
    
   constructor(props) {
+
       super(props);
-      this.onGetOrdersClick = this.onGetOrdersClick.bind(this);
-      this.onGetProductsClick = this.onGetProductsClick.bind(this);
-      this.onSaveChanges = this.onSaveChanges.bind(this);
-      this.renderEditable = this.renderEditable.bind(this);
-      this.onEndUseChange = this.onEndUseChange.bind(this);
-      this.onReceiptNumberChange = this.onReceiptNumberChange.bind(this);
-      this.onCustomerNameChange = this.onCustomerNameChange.bind(this);
-      this.onSellingPriceChange = this.onSellingPriceChange.bind(this);
-      this.getTdProps = this.getTdProps.bind(this);
+
+      // Bind methods to this instance
+      this.onAddOrder = this.onAddOrder.bind(this);
+      this.onFileSelected = this.onFileSelected.bind(this);
+      this.onFileLoaded = this.onFileLoaded.bind(this);
+      this.getTrProps = this.getTrProps.bind(this);
+      this.subComponent = this.subComponent.bind(this);
+      this.showResponse = this.showResponse.bind(this);
+
+      this.selectedFile = null;
       
       const cadFormat = new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'CAD' })
       
       this.orderColumns = [
+        { expander: true},
         { Header: "Orders", 
            columns: [
-              { Header: 'Order Number',     accessor: 'orderNumber',  Cell: props => ( <a href={"/orderitems?orderNumber=" + props.value}>{props.value}</a> )}, 
-              { Header: 'Date',             accessor: 'date',             },
-              { Header: 'Account',          accessor: 'account',          },
-              { Header: 'Subtotal',         accessor: 'subtotal',         },
-              { Header: 'Tax',              accessor: 'tax',      Cell: props => cadFormat.format(props.value) },
-              { Header: 'Shipping',         accessor: 'shipping', Cell: props => cadFormat.format(props.value) },
+              { Header: 'Order Number',     accessor: 'orderNumber',  style: { textAlign: "center" }, Cell: props => ( <a href={"/orderitems?orderNumber=" + props.value}>{props.value}</a> )}, 
+              { Header: 'Date',             accessor: 'date',         style: { textAlign: "right"  },    },
+              { Header: 'Account',          accessor: 'account',      style: { textAlign: "center" },    },
+              { Header: 'Subtotal',         accessor: 'subtotal',     style: { textAlign: "right" }, Cell: props => cadFormat.format(props.value) },
+              { Header: 'Tax',              accessor: 'tax',          style: { textAlign: "right" }, Cell: props => cadFormat.format(props.value) },
+              { Header: 'Shipping',         accessor: 'shipping',     style: { textAlign: "right" }, Cell: props => cadFormat.format(props.value) },
               { Header: 'Shipping Address', accessor: 'shippingAddress',  },
            ]
-        }
-      ];
-
-      this.productColumns = [
-        { Header: "Products", 
-          columns: [
-             { Header: 'Order Number',     accessor: 'orderNumber',  style: { textAlign: "left" }  }, 
-             { Header: 'Description',      accessor: 'description',  style: { textAlign: "left" }  }, 
-             { Header: 'Cost',             accessor: 'costPrice',    style: { textAlign: "right" }, Cell: cellInfo => cadFormat.format(cellInfo.value) }, 
-             { Header: 'Tax',              accessor: 'tax',          style: { textAlign: "right" }, Cell: cellInfo => cadFormat.format(cellInfo.value) }, 
-             { Header: 'Shipping',         accessor: 'shipping',     style: { textAlign: "right" }, Cell: cellInfo => cadFormat.format(cellInfo.value) },
-           ]
-        },
-        { Header: "Destination", 
-          columns: [
-            { Header: 'End Use',
-              accessor: 'endUse',  
-              Cell: (props) => {
-                      
-                         if (props.index == 0) {
-                             console.log(props.value);
-                         }
-                         return (
-                          <Form.Control as="select" value={props.value}  onChange={ (ev)=> {this.onEndUseChange(ev, props.index)} } >
-                             <option value="INSTOCK">In stock</option>
-                             <option value="SOLD">Sold</option>
-                             <option value="PERSONAL">Personal use</option>
-                             <option value="DEMO">Demo</option>
-                             <option value="SAMPLE">Sample</option>
-                          </Form.Control>
-                             )
-                         }
-            
-                  
-             },
-             { Header: 'Customer', accessor: 'customerName',
-                  Cell: (props) => (
-                          <Form.Control defaultValue={props.value} onChange={ (ev)=> {this.onCustomerNameChange(ev, props.index)} }>
-                          </Form.Control>
-                          )
-                  },
-             { Header: 'Selling Price', 
-               accessor: 'sellingPrice', 
-                      Cell: (props) => (
-                              <Form.Control defaultValue={props.value} onChange={ (ev)=> {this.onSellingPriceChange(ev, props.index)}} >
-                              </Form.Control>
-                              )
-             },
-             { Header: 'Receipt', 
-               accessor: 'receiptNumber',
-                  Cell: (props) => (
-                     <Form.Control defaultValue={props.value != null ? props.value:undefined} onChange={ (ev)=> {this.onReceiptNumberChange(ev, props.index)}}   >
-                     </Form.Control>
-                     )
-             }
-         ]
         }
       ];
 
@@ -99,131 +48,126 @@ class Nuskin extends Component {
      
   componentDidMount() {
       const ev = 1;
-      this.onGetOrdersClick(ev);
-  }
-  
-
-  onSaveChanges() {
-     
-  }
-
-  onEndUseChange(ev, index) {
-      
-      console.log("End use change for row " + index + " to " + ev.target.value);
-     
-      var data = [...this.state.data];
-      data[index].endUse = ev.target.value;
-      var columns = [...this.productColumns];
-      this.setState({ data, columns } );
-      
-      
-      // Maybe Want to: 
-      //  Enable/disable customer/selling price/receipt controls
-      //  Enable SAVE button
-      // Validate selling price, receipt, customer
-      // Update database
-  }
-  
-  onReceiptNumberChange(ev, index) {
-      console.log("Receipt number changed for row " + index)
-      var data = [...this.state.data];
-      data[index].receiptNumber = ev.target.value;
-      this.setState(data);
-  }
-  onCustomerNameChange(ev, index) {
-      console.log("Customer name changed for row " + index)
-  }
-  onSellingPriceChange(ev, index) {
-      console.log("Selling price changed for row " + index)
-  }
-
-  
-  renderEditable(cellInfo) {
-      const cadFormat = new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'CAD' })
-
-      return (
-        <div
-          style={{ backgroundColor: "#fafafa" }}
-          contentEditable
-          suppressContentEditableWarning
-          onBlur={e => {
-            const data = [...this.state.data];
-            data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
-            this.setState({ data });
-          }}
-          dangerouslySetInnerHTML={{
-            __html: cadFormat.format(this.state.data[cellInfo.index][cellInfo.column.id])
-          }}
-        />
-      );
-    }
-  
-    
-  onGetOrdersClick(ev) {
-      console.log("form onSubmit");
-
-      this.setState ({columns : [...this.orderColumns] });
-
       
       fetch("/orders")
       .then( res => res.json())
       .then( (res) => {this.update(res) } );
-     
       
   }
-  
-  onGetProductsClick(orderNumber) {
-      this.setState ({columns : [...this.productColumns] });
-      
-      fetch("/products?orderNumber=" + orderNumber)
-      .then( res => res.json())
-      .then( (res) => {this.update(res) } );
-      
-  }
-    
+
+   
   update(res) {
       this.setState ({ data : res });
   }
   
   
-  getTdProps(state, rowInfo, column, instance) {
+  onFileLoaded(ev) {
+      console.log("File loaded");
       
-      return {
-          onDoubleClick: (e, handleOriginal) => {
-              
-            if (column.id == 'orderNumber') {
-                console.log("Order number " + rowInfo.row.orderNumber + "was clicked");
-                
-                this.onGetProductsClick(rowInfo.row.orderNumber);
-                
-            }
+      // Do something with ev.target.result 
+      // Post it to my application
+      
+      //axios.post("/orderfileupload", formData) // { file: ev.target.result} )
+      axios.post("/orderfileupload", { file: ev.target.result} )
+      .then(function (response) {
+          console.log(response);
+        })
+      .catch(function (error) {
+          console.log(error);
+        });
+      
+  }
   
 
-            // IMPORTANT! React-Table uses onClick internally to trigger
-            // events like expanding SubComponents and pivots.
-            // By default a custom 'onClick' handler will override this functionality.
-            // If you want to fire the original onClick handler, call the
-            // 'handleOriginal' function.
-            if (handleOriginal) {
-              handleOriginal();
-            }
+  
+  showResponse(res) {
+      console.log(res.data);
+  }
+  
+  
+  onAddOrder() {
+      
+      // Options: upload file, copy from clipboard, paste into edit box
+      if (this.selectedFile != null) {
+          if (window.confirm("Uploading order " + this.selectedFile.name)) {
+
+              /*
+              var reader = new FileReader();
+              reader.onload = this.onFileLoaded;
+              
+              reader.readAsDataURL(this.selectedFile);
+              */
+    
+              var formData = new FormData();
+              formData.append('file', this.selectedFile);
+              formData.append('name', 'thefilename');
+              formData.append('description', 'anupload');
+              
+              axios.post("/orderfileupload", formData) // { file: ev.target.result} )
+//              .then(res => res.json())
+              .then(res => {
+                      this.showResponse(res);
+                    }
+              );
           }
+          
       }
+      
+  }
+  
+  onFileSelected(ev) {
+      this.selectedFile = ev.target.files[0];
+  }
+  
+  
+  getTrProps(state, rowInfo) {
+      
+      // yellow "#EDF7A3"  green "#CEF7A3",
+      
+      if (rowInfo && rowInfo.original && !rowInfo.original.hasUnsoldItems) {
+          return {
+              style : { 
+                backgroundColor: "#CEF7A3"
+              },
+            }
+      }
+      else {
+          return {};
+      }
+      
+  }
+  
+  
+  subComponent(rowInfo) {
+     if (rowInfo && rowInfo.row && rowInfo.row.orderNumber) { 
+          return (
+              <div style={{ padding: "20px" }}>
+                  <Products orderNumber={rowInfo.row.orderNumber} /> 
+             </div>);
+     }
+     else {
+         return ({});
+     }
+      
   }
   
   render() {
 
       const data = this.state.data ? this.state.data : undefined;
-      const columns = this.state.columns;
-      
+   
       return (
           <div>
               <form style={{margin: '10px'}}>
-                  <Button onClick={this.onGetOrdersClick}>Orders</Button>
-                  <Button onClick={this.onSaveChanges}>Save changes</Button>
-              </form>
-              <ReactTable data={data} columns={columns} defaultPageSize={10} className="-striped -highlight" 
-                          getTdProps={this.getTdProps} />
+              <input type="file" onChange={this.onFileSelected} />
+              <Button onClick={this.onAddOrder}>Add Order</Button>
+              </form>         
+              <ReactTable data={data} 
+                          columns={this.orderColumns} 
+                          defaultPageSize={50} 
+                          className="-striped -highlight" 
+                          getTrProps={this.getTrProps} 
+                          SubComponent = { row =>  {  return this.subComponent(row); } }
+                  />
           </div>
     );
   }
--- src/Products.jsx
+++ src/Products.jsx
@@ -1,28 +1,31 @@
 import React,  { Component } from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import './Nuskin.css';
 import { Tab, Tabs, Form, FormGroup, FormControl, Control, ControlLabel, Checkbox, Button, ListGroup, ListGroupItem, InputGroup } from 'react-bootstrap'
 import ReactTable from "react-table";
-import 'react-table/react-table.css'
-
+import 'react-table/react-table.css';
+import CurrencyInput from 'react-currency-input';
 class Products extends Component {
    
   constructor(props) {
       super(props);
-      this.onGetProductsClick = this.onGetProductsClick.bind(this);
       this.onSaveChanges = this.onSaveChanges.bind(this);
+      this.onDiscardChanges = this.onDiscardChanges.bind(this);
       this.onEndUseChange = this.onEndUseChange.bind(this);
       this.onReceiptNumberChange = this.onReceiptNumberChange.bind(this);
       this.onCustomerNameChange = this.onCustomerNameChange.bind(this);
       this.onSellingPriceChange = this.onSellingPriceChange.bind(this);
-      this.getTdProps = this.getTdProps.bind(this);
+      this.getTrProps = this.getTrProps.bind(this);
+      this.refresh = this.refresh.bind(this);
       
       const cadFormat = new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'CAD' })
       
       this.productColumns = [
-        { Header: "Products", 
+        { Header: "Order Number: " + props.orderNumber, 
           columns: [
-             { Header: 'Order Number',     accessor: 'orderNumber',  style: { textAlign: "left" }  }, 
+             { Header: 'Item',             accessor: 'id',           style: { textAlign: "center" }  }, 
+             { Header: 'SKU',              accessor: 'sku',          style: { textAlign: "center" }  }, 
              { Header: 'Description',      accessor: 'description',  style: { textAlign: "left" }  }, 
              { Header: 'Cost',             accessor: 'costPrice',    style: { textAlign: "right" }, Cell: cellInfo => cadFormat.format(cellInfo.value) }, 
              { Header: 'Tax',              accessor: 'tax',          style: { textAlign: "right" }, Cell: cellInfo => cadFormat.format(cellInfo.value) }, 
@@ -34,10 +37,6 @@ class Products extends Component {
             { Header: 'End Use',
               accessor: 'endUse',  
               Cell: (props) => {
-                      
-                         if (props.index == 0) {
-                             console.log(props.value);
-                         }
                          return (
                           <Form.Control as="select" value={props.value}  onChange={ (ev)=> {this.onEndUseChange(ev, props.index)} } >
                              <option value="INSTOCK">In stock</option>
@@ -48,26 +47,26 @@ class Products extends Component {
                           </Form.Control>
                              )
                          }
-            
-                  
              },
              { Header: 'Customer', accessor: 'customerName',
                   Cell: (props) => (
-                          <Form.Control defaultValue={props.value} onChange={ (ev)=> {this.onCustomerNameChange(ev, props.index)} }>
+                          <Form.Control value={props.value != null ? props.value:undefined} onChange={ (ev)=> {this.onCustomerNameChange(ev, props.index)} }>
                           </Form.Control>
                           )
                   },
              { Header: 'Selling Price', 
                accessor: 'sellingPrice', 
                       Cell: (props) => (
-                              <Form.Control defaultValue={props.value} onChange={ (ev)=> {this.onSellingPriceChange(ev, props.index)}} >
-                              </Form.Control>
+                              <div>$
+                              <CurrencyInput value={props.value != null ? props.value:undefined} onChangeEvent={ (ev)=> {this.onSellingPriceChange(ev, props.index)}}>
+                              </CurrencyInput>
+                              </div>
                               )
              },
              { Header: 'Receipt', 
                accessor: 'receiptNumber',
                   Cell: (props) => (
-                     <Form.Control defaultValue={props.value != null ? props.value:undefined} onChange={ (ev)=> {this.onReceiptNumberChange(ev, props.index)}}   >
+                     <Form.Control value={props.value != null ? props.value:undefined} onChange={ (ev)=> {this.onReceiptNumberChange(ev, props.index)}}   >
                      </Form.Control>
                      )
              }
@@ -80,27 +79,72 @@ class Products extends Component {
      
       
   }
+
+//  <Form.Control value={props.value != null ? props.value:undefined} onChange={ (ev)=> {this.onSellingPriceChange(ev, props.index)}} >
+//  </Form.Control>
+
+  
+  refresh() {
+      fetch("/products?orderNumber=" + this.props.orderNumber)
+      .then( res => res.json())
+      .then( (res) => {this.update(res) } );
+  }
+
+  update(res) {
       
-     
+      for (var i=0;  i<res.length; i++ ) {
+         res[i].modified=false;
+      }
+      this.setState ({ data : res });
+  }
+ 
+
   componentDidMount() {
-      const ev = 1;
-      this.onGetProductsClick(this.props.orderNumber);
+      this.refresh();
   }
   
-
+  onDiscardChanges() {
+      // just re-read the data
+      this.refresh();
+  }
+  
   onSaveChanges() {
-     
+      // Find all modified items and POST back to the server
+      
+      var data = [...this.state.data];
+      
+      var modifiedRows = new Array();
+      
+      for (var i=0; i<data.length; i++) {
+          if (data[i].modified) {
+             var modifiedRow = { ...data[i] }; // take a copy 
+             delete modifiedRow.modified;  // so as not to confuse the server 
+             modifiedRows.push(modifiedRow);
+          }
+      }
+      
+      fetch('/updateitems', {
+          method: 'PUT',   // Updates use PUT request
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(modifiedRows),
+      })
+      // When that completes, refresh everything
+      .then( () => { this.refresh();} );
+      
+      
+      
   }
 
   onEndUseChange(ev, index) {
       
-      console.log("End use change for row " + index + " to " + ev.target.value);
-     
       var data = [...this.state.data];
       data[index].endUse = ev.target.value;
+      data[index].modified = true;
       var columns = [...this.productColumns];
       this.setState({ data, columns } );
-      
       
       // Maybe Want to: 
       //  Enable/disable customer/selling price/receipt controls
@@ -110,71 +154,67 @@ class Products extends Component {
   }
   
   onReceiptNumberChange(ev, index) {
-      console.log("Receipt number changed for row " + index)
+      // State data is immutable, have to take a copy then setState again
       var data = [...this.state.data];
+      var columns = [...this.productColumns];
       data[index].receiptNumber = ev.target.value;
-      this.setState(data);
+      data[index].modified = true;
+      this.setState({data, columns});
   }
-  onCustomerNameChange(ev, index) {
-      console.log("Customer name changed for row " + index)
-  }
-  onSellingPriceChange(ev, index) {
-      console.log("Selling price changed for row " + index)
-  }
-
-
   
-  onGetProductsClick(orderNumber) {
-      this.setState ({columns : [...this.productColumns] });
-      
-      fetch("/products?orderNumber=" + orderNumber)
-      .then( res => res.json())
-      .then( (res) => {this.update(res) } );
-      
+  onCustomerNameChange(ev, index) {
+      var data = [...this.state.data];
+      var columns = [...this.productColumns];
+      data[index].customerName = ev.target.value;
+      data[index].modified = true;
+      this.setState({data, columns});
+  }
+  
+  onSellingPriceChange(ev, index) {
+      var data = [...this.state.data];
+      var columns = [...this.productColumns];
+      data[index].sellingPrice = ev.target.value;
+      data[index].modified = true;
+      this.setState({data, columns});
   }
     
-  update(res) {
-      this.setState ({ data : res });
-  }
-  
-  
-  getTdProps(state, rowInfo, column, instance) {
-      
-      return {
-          onDoubleClick: (e, handleOriginal) => {
-              
-            if (column.id == 'orderNumber') {
-                console.log("Order number " + rowInfo.row.orderNumber + "was clicked");
-                
-                this.onGetProductsClick(rowInfo.row.orderNumber);
-                
-            }
-  
 
-            // IMPORTANT! React-Table uses onClick internally to trigger
-            // events like expanding SubComponents and pivots.
-            // By default a custom 'onClick' handler will override this functionality.
-            // If you want to fire the original onClick handler, call the
-            // 'handleOriginal' function.
-            if (handleOriginal) {
-              handleOriginal();
-            }
+  
+  getTrProps(state, rowInfo) {
+      
+      // yellow "#EDF7A3"  green "#CEF7A3",
+      var bkcolor = "#CEF7A3";
+      var retval = {};
+      
+      
+      if (rowInfo && rowInfo.row ) {
+          // If anything has been changed we are yellow 
+          if (rowInfo.original.modified) {
+              bkcolor = "#EDF7A3";
+              retval = { style : { backgroundColor: bkcolor  } }
           }
+          // If the enduse is set we are GREEN
+          else if (rowInfo.row.endUse != 'INSTOCK') { 
+              bkcolor = "#CEF7A3";
+              retval = { style : { backgroundColor: bkcolor  } }
+          }
+          // Otherwise drop through
       }
+
+      // If not a product row (no rowInfo/rowInfo.row), not modified and still still instock, don't change the style
+      return retval;
   }
   
   render() {
 
       const data = this.state.data ? this.state.data : undefined;
-      const columns = this.state.columns;
-      
       return (
               <div>
               <form style={{margin: '10px'}}>
                   <Button onClick={this.onSaveChanges}>Save changes</Button>
+                  <Button onClick={this.onDiscardChanges}>Discard changes</Button>
               </form>         
-              <ReactTable data={data} columns={columns} defaultPageSize={10} className="-striped -highlight" 
-                          getTdProps={this.getTdProps} />
+              <ReactTable minRows="1" data={data} columns={this.productColumns} getTrProps={this.getTrProps} defaultPageSize={20} className="-striped -highlight" />
               </div>
     );
   }
