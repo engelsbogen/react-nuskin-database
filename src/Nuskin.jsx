817816a770fe91569f836b253aafd9b9f504198f
acamm <aijun.camm@gmail.com>
2019-03-22 13:16:19 -0300

    Allow upload of multiple order files

--- src/Nuskin.jsx
+++ src/Nuskin.jsx
@@ -9,6 +9,12 @@ import axios from "axios";
 import Products from "Products";
 import NewSKUs from "NewSKUs";
 import NuskinAlert from "NuskinAlert";
+import _ from "lodash";
+
+
+var lodash = _;
+
+const cadFormat = new Intl.NumberFormat('en-CA', { style: 'currency', currency: 'CAD' })
 
 class Nuskin extends Component {
    
@@ -27,35 +33,89 @@ class Nuskin extends Component {
       this.handleShow = this.handleShow.bind(this);
       this.handleUploadError = this.handleUploadError.bind(this);
       this.refresh = this.refresh.bind(this);
+      this.doNextUpload = this.doNextUpload.bind(this);
+      this.getTotal = this.getTotal.bind(this);
+      this.makeOrderColumns = this.makeOrderColumns.bind(this);
       
       this.selectedFile = null;
+      this.nextUploadIndex  = 0;
       
-      const cadFormat = new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'CAD' })
+      this.makeOrderColumns();
       
+      this.state =  { data: null,
+          columns : this.orderColumns,
+          newSKUs: null,
+          show : false };
+
+
+      
+  }
+  
+  
+  
+  makeOrderColumns() {
+      
+
       this.orderColumns = [
         { expander: true},
         { Header: "Orders", 
            columns: [
-              { Header: 'Order Number',  width: 100,   accessor: 'orderNumber',  style: { textAlign: "center" }, Cell: props => ( <a href={"/orderfiledownload?orderNumber=" + props.value}>{props.value}</a> )}, 
-              { Header: 'Date',          width: 100,    accessor: 'date',         style: { textAlign: "right"  },    },
-              { Header: 'Account',       width: 100,   accessor: 'account',      style: { textAlign: "center" },    },
+              { Header: 'Month',         width: 120,  accessor: 'month',        style: { textAlign: "center" } }, 
+              { Header: 'Order Number',  
+                  Aggregated: ()=> { return  (<span/>); },  // Empty in aggregated row
+                  width: 100,  accessor: 'orderNumber',  style: { textAlign: "center" }, Cell: props => ( <a href={"/orderfiledownload?orderNumber=" + props.value}>{props.value}</a> )}, 
+              { Header: 'Date',
+                  Aggregated: ()=> { return  (<span/>); },  // Empty in aggregated row
+                  width: 100,  accessor: 'date',         style: { textAlign: "right"  },    },
+              { Header: 'Account',       width: 120, 
+                  id: 'accountDetails',
+                  Aggregated: ()=> { return  (<span/>); },  // Empty in aggregated row
+                  accessor: 
+                      d => d.account + "\n" + d.accountName,     
+                  style: { textAlign: "center", whiteSpace: 'unset' },    },
               { Header: 'Total',         width: 100,
                   id : 'total',
-                  accessor: d => d.subtotal + d.tax + d.shipping,     
+                  // Aggregated row sums the total orders for the month
+                  aggregate: vals => lodash.sum(vals),
+             
+                  accessor: d => d.subtotal + d.tax + d.shipping,   
+                  
+                  Footer: (
+                          <span>
+                            <strong>
+                            { this.getTotal()}
+                            </strong>
+                          </span>
+                        ),
+       
                   style: { textAlign: "right" }, 
                   Cell: props => cadFormat.format(props.value) },
-              { Header: 'Item Summary', accessor: 'itemSummary',  },
+              { Header: 'Item Summary', accessor: 'itemSummary',
+                  Aggregated: ()=> { return  (<span/>); },  // Empty in aggregated row
+                  // React-table cells are set to {white-space: nowrap}. To allow wrapping in one cell, unset this style
+                  // For react we have to change to whiteSpace
+                  style: { 'whiteSpace': 'unset'} },
            ]
         }
+        
       ];
 
-      this.state =  { data: null,
-                      columns : this.orderColumns,
-                      newSKUs: null,
-                      show : false };
-      
+      return this.orderColumns;
   }
-      
+    
+
+  getTotal() {
+      if (this.state && this.state.data) {
+          var yearTotal = lodash.sum(lodash.map(this.state.data, d => d.subtotal + d.tax + d.shipping));
+          
+          // Format as currency
+          return cadFormat.format(yearTotal);
+      }
+      else {
+          return 0;
+      }
+  }
+
      
   componentDidMount() {
       this.refresh();
@@ -88,26 +148,35 @@ class Nuskin extends Component {
 
   
   showResponse(res) {
-      var showDialog = res.data && res.data.length > 0;
+      var showDialog = res.data && res.data.unknownItems.length > 0;
          
       // If the were no problems, refresh the table
       if (!showDialog) {
-          NuskinAlert.showAlert("Order file uploaded successfully");
-          this.refresh();
+          
+          NuskinAlert.showAlert("Order file " + this.selectedFile[this.nextUploadIndex].name + " uploaded successfully");
+
+          this.nextUploadIndex++;
+          if (this.nextUploadIndex < this.selectedFile.length) {
+              this.doNextUpload();
+          }
+          else {
+              this.setState ( { newSKUs: [], show: false } );
+              this.refresh();
+          }
       }
       else {
-          this.setState( { newSKUs: res.data, show: true } );
+          this.setState( { newSKUs: res.data.unknownItems, show: true } );
       }
       
   }
   
   onHideDialog(skusUpdated) {
       
-      this.setState({show: false});
+      this.setState({ newSKUs: [], show: false});
       
       if (skusUpdated) {
           // Retry sending the order
-          this.onAddOrder(false);
+          this.doNextUpload();
       }
       
   }
@@ -121,28 +190,62 @@ class Nuskin extends Component {
       NuskinAlert.showAlert(err.response.data.message);
   }
   
+  doNextUpload() {
+      
+      var formData = new FormData();
+      formData.append("file", this.selectedFile[this.nextUploadIndex]);
+      formData.append('name', 'thefilename');
+      formData.append('description', 'anupload');
+      axios.post("/orderfileupload", formData) 
+          .then(res => { this.showResponse(res); })
+          .catch( err=> { this.handleUploadError(err); });
+      
+  }
+  
   onAddOrder(askConfirmation) {
       
       // Options: upload file, copy from clipboard, paste into edit box
       if (this.selectedFile != null) {
-          if ( !askConfirmation || window.confirm("Uploading order " + this.selectedFile.name)) {
+          if ( !askConfirmation || window.confirm("Uploading orders ")) {
 
+
+              this.nextUploadIndex = 0;
+              
+              this.doNextUpload();
+              
+//              for (var x = 0; x < this.selectedFile.length; x++) {
+//                  var formData = new FormData();
+//                  formData.append("file", this.selectedFile[x]);
+//                  formData.append('name', 'thefilename');
+//                  formData.append('description', 'anupload');
+//                  axios.post("/orderfileupload", formData) 
+//                      .then(res => { this.showResponse(res); })
+//                      .catch( err=> { this.handleUploadError(err); });
+//              }
+              
+              
+              /*              
               var formData = new FormData();
-              formData.append('file', this.selectedFile);
+              for (var x = 0; x < this.selectedFile.length; x++) {
+                  formData.append("file[]", this.selectedFile[x]);
+              }
               formData.append('name', 'thefilename');
               formData.append('description', 'anupload');
               
-              axios.post("/orderfileupload", formData) 
+              axios.post("/orderfileuploadmulti", formData) 
               .then(res => { this.showResponse(res);  })
               .catch( err=> { this.handleUploadError(err); });
+              */
           }
           
+              
+              
       }
       
   }
   
   onFileSelected(ev) {
-      this.selectedFile = ev.target.files[0];
+      this.selectedFile = ev.target.files;
   }
   
   
@@ -182,18 +285,27 @@ class Nuskin extends Component {
       const data = this.state.data ? this.state.data : undefined;
     
       var show = (this.state.newSKUs && this.state.newSKUs.length > 0);
-
+      
+      var orderFilename = "";
+      if (this.selectedFile && this.nextUploadIndex < this.selectedFile.length) {
+          orderFilename = this.selectedFile[this.nextUploadIndex].name;
+      }
+      
+      this.makeOrderColumns();
+      
       return  (
            <>      
-              <NewSKUs data={this.state.newSKUs} show={ this.state.show } onHide={this.onHideDialog} /> 
+              <NewSKUs filename={ orderFilename } data={this.state.newSKUs} show={ this.state.show } onHide={this.onHideDialog} /> 
               <form style={{margin: '10px'}}>
-              <div className="file btn btn-primary"><input type="file" onChange={this.onFileSelected} /></div>
+              <div className="file btn btn-primary"><input type="file" multiple onChange={this.onFileSelected} /></div>
               <Button onClick={() => {this.onAddOrder(true);} }>Add Order</Button>
            
               </form>         
               <ReactTable data={data} 
                           columns={this.orderColumns} 
+                          pivotBy={["month"]} 
                           defaultPageSize={50} 
+                          minRows="1"
                           className="-striped -highlight" 
                           getTrProps={this.getTrProps} 
                           SubComponent = { row =>  {  return this.subComponent(row); } }
@@ -202,6 +314,7 @@ class Nuskin extends Component {
           );
   }
 }
+
 
 
   
