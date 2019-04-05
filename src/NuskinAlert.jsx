bcd55d7cd3426221ca4139e8edd50554fac58548
acamm <aijun.camm@gmail.com>
2019-04-05 16:38:49 -0300

    now uses a promise, can be used to wait for the user to close the dialog

--- src/NuskinAlert.jsx
+++ src/NuskinAlert.jsx
@@ -10,7 +10,21 @@ export class NuskinAlert extends Component {
     static showAlert(message) {  
         
         NuskinAlert.theAlert.setMessage(message);
+        NuskinAlert.theAlert.setType("alert");
         NuskinAlert.theAlert.setState( { show: true } );
+        return NuskinAlert.theAlert.getPromise();
+    }
+    
+    static showConfirm(message) {
+        
+        NuskinAlert.theAlert.setMessage(message);
+        NuskinAlert.theAlert.setType("confirm");
+        NuskinAlert.theAlert.setState ( {show: true } );
+        return NuskinAlert.theAlert.getPromise();
+    }
+    
+    static getExitCode() {
+        return NuskinAlert.theAlert.getExitCode();
     }
     
     constructor(props) {
@@ -20,12 +34,25 @@ export class NuskinAlert extends Component {
         
         this.handleShow = this.handleShow.bind(this);
         this.handleClose = this.handleClose.bind(this);
-
+        this.onOK = this.onOK.bind(this);
+        this.onCancel = this.onCancel.bind(this);
+        this.getExitCode = this.getExitCode.bind(this);
+        this.setType = this.setType.bind(this);
+        this.getPromise = this.getPromise.bind(this);
+        
         this.state = { show: false };
         this.message = '';
+        this.exitCode = 0;
         
     }
     
+    getPromise() {
+        return this.promise;
+    }
+    
+    getExitCode() {
+        return this.exitCode;
+    }
     handleClose() {
         this.setState({ show: false });
     }
@@ -38,14 +65,46 @@ export class NuskinAlert extends Component {
         this.message = msg;
     }
     
+    setType(dlgType) {
+        this.dlgType=dlgType;
+        this.exitCode = 0;
+        this.promise = new Promise((resolve, reject) => {this.resolve = resolve} );
+    }
+    
+    onOK() {
+        this.exitCode = 0;
+        this.handleClose();
+        this.resolve(0);
+    }
+    onCancel() {
+        this.exitCode = 1;
+        this.handleClose();
+        this.resolve(1);
+    }
+    
     render () {
+
+        var buttons  = (this.dlgType == 'confirm') ?
+              (<span>
+                  <Button onClick={this.onOK}>OK</Button>
+                  <Button onClick={this.onCancel}>Cancel</Button>
+                 </span>)
+            :
+             (<span>
+                    <Button onClick={this.onOK}>OK</Button>
+              </span>);
+            
+        ;
+        
+        
         return (
         <Modal show={this.state.show}  onHide={this.handleClose} > 
-          <Modal.Header closeButton>
+          <Modal.Header >
           <Modal.Title>Alert</Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <p> {this.message} </p>
+            {buttons}
           </Modal.Body>
         </Modal>
         );
